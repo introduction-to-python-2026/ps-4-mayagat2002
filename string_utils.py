e06c68a225f546669dba3e799196299f8f26f672
mayagat2002 <mayagat2002@gmail.com>
2025-11-13 09:46:13 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,21 @@
-def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
 
+def split_before_each_uppercases(formula):
+
+    import re
+    parts = re.findall('[A-Z][^A-Z]*', formula)
+    return parts
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    
+    import re
+    match = re.search(r'(.*?)(\d.*)', formula)
+    
+    if match:
+        return list(match.groups())
+    else:
+        return [formula, ""]
+
+
+def split_chem_formula(formula):
+    import re
+    return re.findall(r'([A-Z][a-z]?\d*)', formula)
